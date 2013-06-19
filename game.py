942dad27cf64552e7863a338fa326e29b8186702
John O'Leary <olear121@umn.edu>
2013-06-19 12:19:45 -0500

    Added a player file. Building a framework still.

--- game.py
+++ game.py
@@ -6,5 +6,17 @@ import dice
 
 dice_list = [dice.Dice() for i in range(6)]
 
+print "How many players?"
+number_of_players = input()
+
+player_list = [p]
+
 for i in range(6):
 	print dice_list[i].roll()
+
+game_over = False
+
+while not game_over:
+	
+	print "hi"
+	game_over = True
